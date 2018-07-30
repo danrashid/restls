56e5e99ab99dd1bfc14d420890bce53d6ba9aab8
Dan Rashid <dan@pecsafety.com>
2018-07-29 19:24:25 -0500

    Type-check setCollection

--- src/setCollection.ts
+++ src/setCollection.ts
@@ -1,3 +1,5 @@
-export const setCollection = (key: string, value: Array<Object> | Object) => {
+import { IBody } from "./interfaces/body";
+
+export const setCollection = (key: string, value: Array<IBody> | Object) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
