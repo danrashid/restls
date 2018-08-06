c020e7a6390c3ea1b5b4da80ad390aeb66787fd6
Dan Rashid <dan@pecsafety.com>
2018-08-05 19:57:00 -0500

    Change error message

--- src/setCollection.ts
+++ src/setCollection.ts
@@ -9,7 +9,7 @@ export const setCollection = (key: string, value: Array<IMember>) => {
     localStorage.setItem(key, JSON.stringify(value));
   } else {
     throw new Error(
-      `Failed to set collection for key "${key}" because it contains duplicate IDs.`
+      `Failed to set collection "${key}" because it contains duplicate IDs.`
     );
   }
 };
